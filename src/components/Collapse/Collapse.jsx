3878726fd1a01a1466472344e0fca4ee894755b3
empostigo <emmanuel.postigo@tutanota.com>
2023-12-01 21:24:02 +0100

    Create Collapse component

---
+++ src/components/Collapse/Collapse.jsx
@@ -0,0 +1,30 @@
+import aboutData from "../../data/aboutData"
+import collapse from "./Collapse.module.scss"
+
+const Collapse = (props) => {
+  return (
+    <details className={collapse.details}>
+      <summary className={collapse.summary}>{props.title}</summary>
+        {props.children}
+    </details>
+  )
+}
+
+const AboutCollapse = (props) => {
+  return (
+    <Collapse title={props.title}>
+      <p className={collapse.description}>{props.description}</p>
+    </Collapse>
+  )
+}
+
+export const AboutSection = () => {
+  const collapses = aboutData.map(data => <AboutCollapse key={data.id}
+                                                         title={data.title} 
+                                                         description={data.description} />)
+  return (
+    <section className={collapse.section}>
+      {collapses}
+    </section>
+  )
+}
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,10 +1,12 @@
 import Banner from "../../components/Banner/Banner"
+import { AboutSection } from "../../components/Collapse/Collapse"
 import about from "./About.module.scss"
 
 const About = () => {
   return (
     <main className={about.about}>
       <Banner />
+      <AboutSection />
     </main>
   )
 }
